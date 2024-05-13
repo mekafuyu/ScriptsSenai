9d585ea7b77ec49232e1d422e0d163a9d12a306f
mekafuyu <mayconbrunob@hotmail.com>
2024-05-13 09:15:55 -0300

    PathConfig

--- InstallDeps.ps1
+++ InstallDeps.ps1
@@ -1,5 +1,12 @@
+echo "Inciando Criação de Pasta"
 mkdir $env:USERPROFILE\dev\ -ea 0
+echo "Pasta Criada"
 
-echo "Inciando"
 
+echo "Inciando Extração"
 Expand-Archive -Path .\flutter_windows_3.19.6-stable.zip -DestinationPath $env:USERPROFILE\dev\
+echo "Extraido"
+
+echo "Configurando Path"
+$env:Path += ';%USERPROFILE%\dev\flutter\bin'
+echo "Path Configurado'"
